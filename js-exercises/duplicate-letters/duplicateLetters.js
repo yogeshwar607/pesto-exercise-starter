ae1c8d6ca105af193b43586c5f6eef37afa4bd82
yogi <yogeshwar607@gmail.com>
2020-03-03 19:29:54 +0530

    empty response for PR

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,12 +1,5 @@
 function duplicateLetters(...args) {
-	const strSet = new Set(args[0].split('')); // breaking down string in SET to remove duplicates
-	let count = 1;
-	strSet.forEach((value, valueAgain, set) => {
-		let occurence = args[0].split(value).length - 1;
-		occurence > count ? (count = occurence) : count; // if occurence greater than count , replace count with occurence 
-	});
 
-	return count < 2 ? false : count; // if one occurence then send false
 }
 
 export { duplicateLetters };
